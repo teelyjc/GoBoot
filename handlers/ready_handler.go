9a6361c9391fbe1ecf11e74b19cdbeaebf02df4e
Tannatee Juchan <ju.tannatee2543@gmail.com>
2023-11-19 19:30:17 +0700

    feat: add register handler command after ready

--- handlers/ready_handler.go
+++ handlers/ready_handler.go
@@ -4,12 +4,20 @@ import (
 	"log"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/teelyjc/GoBoot/commands"
+	command "github.com/teelyjc/GoBoot/commands/common"
 )
 
-func ReadyHandler(session *discordgo.Session, ready *discordgo.Ready) {
+func ReadyHandler(
+	session *discordgo.Session,
+	ready *discordgo.Ready,
+	commandManager *commands.CommandManager,
+) {
 	log.Printf(
 		"Logged in as %v#%v",
 		session.State.User.Username,
 		session.State.User.Discriminator,
 	)
+
+	commandManager.RegisterCommand(command.CommandPing())
 }
