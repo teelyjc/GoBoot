e159d624aff54f6f3dd433322be7212babcba0b9
Tannatee Juchan <ju.tannatee2543@gmail.com>
2023-11-19 19:29:47 +0700

    feat: dynamic called command handler

--- handlers/interaction_handler.go
+++ handlers/interaction_handler.go
@@ -1,10 +1,14 @@
 package handlers
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"github.com/bwmarrin/discordgo"
+	"github.com/teelyjc/GoBoot/commands"
+)
 
 func InteractionHandler(
 	session *discordgo.Session,
 	interaction *discordgo.InteractionCreate,
+	commandManager *commands.CommandManager,
 ) {
-
+	commandManager.ExecuteCommand(session, interaction)
 }
